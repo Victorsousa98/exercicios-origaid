d5ee6342140fd0952eb05480f76e629bb4fc3f41
Victor Sousa <victorsousa708@gmail.com>
2023-04-14 20:19:34 -0300

    resolvido

---
+++ js basic exercicio/5/script.js
@@ -0,0 +1,14 @@
+// Declarar uma variável com o seu nome
+const nome = "João Victor Sousa Chagas";
+
+// Declarar uma variável com a sua idade
+const idade = 24;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+const comidaFavorita = "";
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Jussara";
+// Declarar 5 variáveis diferentes sem valores
+var a, b, c, d, e;
