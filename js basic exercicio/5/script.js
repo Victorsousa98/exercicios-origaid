a7dec69029bfa94024bf1b4afd664cfc5e3abc0a
Victor Sousa <victorsousa708@gmail.com>
2023-04-15 11:13:56 -0300

    resolvido

--- js basic exercicio/5/script.js
+++ js basic exercicio/5/script.js
@@ -15,27 +15,54 @@
 
 // Declare uma variável contendo uma string
 
-let namorada = "Klicya"
+let namorada = "Klicya";
 
 // Declare uma variável contendo um número dentro de uma string
 
-let mae = "80"
+let mae = "80";
 
 // Declare uma variável com a sua idade
-let idade = 24
+let idade = 24;
 
 // Declare duas variáveis, uma com seu nome
 // e outra com seu sobrenome e some as mesmas
 
-const nome = "João"
-const sobrenome = "victor"
+const nome = "João";
+const sobrenome = "victor";
 
-console.log(`${nome} ${sobrenome}`)
+console.log(`${nome} ${sobrenome}`);
 
 // Coloque a seguinte frase em uma variável: It's time
 
-let frase = "It's time"
+let frase = "It's time";
 
 // Verifique o tipo da variável que contém o seu nome
 
-console.log(typeof nome)
+console.log(typeof nome);
+
+// Qual o resultado da seguinte expressão?
+var total = 10 + (5 * 2) / 2 + 20;
+console.log(total);
+
+// Crie duas expressões que retornem NaN
+var peso = 25;
+var unidade = "kg";
+
+console.log(peso / unidade);
+console.log("agua" * 24);
+
+// Somar a string '200' com o número 50 e retornar 250
+console.log(+"200" + 50);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var cinco = 5;
+console.log(++cinco);
+console.log(++cinco);
+
+// Como dividir o peso por 2?
+var numero = "80";
+var unidade = "kg";
+var pesoPorDois = numero / 2;
+var peso = pesoPorDois + unidade; // '40kg'
+
+console.log(peso);
