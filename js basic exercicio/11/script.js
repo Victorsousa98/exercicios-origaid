8c9ee7b96ff1ce72afee0ed1382b90e265b587c2
Victor Sousa <victorsousa708@gmail.com>
2023-04-15 14:46:11 -0300

    resolvido

---
+++ js basic exercicio/11/script.js
@@ -0,0 +1,31 @@
+// Por qual motivo o código abaixo retorna com erros?
+{
+  var cor = "preto";
+  const marca = "Fiat";
+  let portas = 4;
+  console.log(cor, marca, portas);
+}
+// console.log(var, marca, portas);
+//pq var não é uma variavel e portas está declarado como let, como está dentro de um escopo, o valor é desconhecido fora dele
+
+// Como corrigir o erro abaixo?
+function somarDois(x) {
+  const dois = 2;
+  return x + dois;
+}
+function dividirDois(x) {
+  const dois = 2;
+  return x / dois;
+}
+somarDois(4);
+dividirDois(6);
+
+// O que fazer para total retornar 500?
+var numero = 50;
+
+for (let numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
