fab05fb7e3c77e5080960f35de52cfb371406824
Victor Sousa <victorsousa708@gmail.com>
2023-04-15 12:29:37 -0300

    resolvido

---
+++ js basic exercicio/7/script.js
@@ -0,0 +1,47 @@
+// Crie uma função para verificar se um valor é Truthy
+function verifica(valor) {
+  return !!valor;
+}
+
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+
+function perimetro(lado) {
+  return lado * 4;
+}
+
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+function nomeCompleto(nome, sobrenome) {
+  return `${nome} ${sobrenome}`;
+}
+
+// Crie uma função que verifica se um número é par
+function verificaPar(numero) {
+  return numero % 2 === 0 ? "é par" : "é impar";
+}
+
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+
+function tipoDado(dado) {
+  return typeof dado;
+}
+
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'scroll' ocorrer.
+addEventListener("scroll", () => console.log("João Victor"));
+
+// Corrija o erro abaixo
+function precisoVisitar(paisesVisitados) {
+  var totalPaises = 193;
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+function jaVisitei(paisesVisitados) {
+  var totalPaises = 193;
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+precisoVisitar(20);
+jaVisitei(20);
