c2c44be03d832b08c29ed6a4032876b5cc1bc8de
Victor Sousa <victorsousa708@gmail.com>
2023-04-14 22:23:11 -0300

    resolvido

--- js basic exercicio/5/script.js
+++ js basic exercicio/5/script.js
@@ -1,14 +1,41 @@
 // Declarar uma variável com o seu nome
-const nome = "João Victor Sousa Chagas";
+// const nome = "João Victor Sousa Chagas";
 
 // Declarar uma variável com a sua idade
-const idade = 24;
+// const idade = 24;
 // Declarar uma variável com a sua comida
 // favorita e não atribuir valor
 
-const comidaFavorita = "";
+// const comidaFavorita = "";
 
 // Atribuir valor a sua comida favorita
-comidaFavorita = "Jussara";
+// comidaFavorita = "Jussara";
 // Declarar 5 variáveis diferentes sem valores
-var a, b, c, d, e;
+// var a, b, c, d, e;
+
+// Declare uma variável contendo uma string
+
+let namorada = "Klicya"
+
+// Declare uma variável contendo um número dentro de uma string
+
+let mae = "80"
+
+// Declare uma variável com a sua idade
+let idade = 24
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+const nome = "João"
+const sobrenome = "victor"
+
+console.log(`${nome} ${sobrenome}`)
+
+// Coloque a seguinte frase em uma variável: It's time
+
+let frase = "It's time"
+
+// Verifique o tipo da variável que contém o seu nome
+
+console.log(typeof nome)
