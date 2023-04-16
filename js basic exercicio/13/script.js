98893fe236419b989a02683991ddadd79fc4c0f2
Victor Sousa <victorsousa708@gmail.com>
2023-04-16 20:17:12 -0300

    resolvido

--- js basic exercicio/13/script.js
+++ js basic exercicio/13/script.js
@@ -1,20 +1,46 @@
-// Retorne no console todas as imagens do site
-const imagens = document.querySelectorAll("img");
-console.log(imagens);
+// // Retorne no console todas as imagens do site
+// const imagens = document.querySelectorAll("img");
+// console.log(imagens);
 
-// Retorne no console apenas as imagens que começaram com a palavra imagem
+// // Retorne no console apenas as imagens que começaram com a palavra imagem
 
-const imagem = document.querySelectorAll('[src^="img/imagem"]');
-console.log(imagem);
+// const imagem = document.querySelectorAll('[src^="img/imagem"]');
+// console.log(imagem);
 
-// Selecione todos os links internos (onde o href começa com #)
-const links = document.querySelectorAll('[href^="#"]');
-console.log(links);
+// // Selecione todos os links internos (onde o href começa com #)
+// const links = document.querySelectorAll('[href^="#"]');
+// console.log(links);
 
-// Selecione o primeiro h2 dentro de .animais-descricao
-const h2 = document.querySelector(".animais-descricao h2");
-console.log(h2);
-// Selecione o último p do site
-const ultimoP = document.querySelectorAll("p")
-console.log(ultimoP[ultimoP.length - 1])
+// // Selecione o primeiro h2 dentro de .animais-descricao
+// const h2 = document.querySelector(".animais-descricao h2");
+// console.log(h2);
+// // Selecione o último p do site
+// const ultimoP = document.querySelectorAll("p")
+// console.log(ultimoP[ultimoP.length - 1])
 
+
+
+// Mostre no console cada parágrado do site
+// Mostre o texto dos parágrafos no console
+const paragrafos = document.querySelectorAll("p")
+paragrafos.forEach(paragrafo => {
+  console.log(paragrafo)
+  console.log(paragrafo.innerText)
+});
+
+
+
+
+// Como corrigir os erros abaixo:
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach( () => {
+  console.log(i++);
+});
+
+imgs.forEach(() => i++);
