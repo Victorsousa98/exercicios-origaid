a54e3d6fc780c36977ec52383a8ff4ca21d566d7
Victor Sousa <victorsousa708@gmail.com>
2023-04-16 20:34:49 -0300

    resolvido

--- js basic exercicio/13/script.js
+++ js basic exercicio/13/script.js
@@ -18,29 +18,46 @@
 // const ultimoP = document.querySelectorAll("p")
 // console.log(ultimoP[ultimoP.length - 1])
 
-
-
 // Mostre no console cada parágrado do site
 // Mostre o texto dos parágrafos no console
-const paragrafos = document.querySelectorAll("p")
-paragrafos.forEach(paragrafo => {
-  console.log(paragrafo)
-  console.log(paragrafo.innerText)
+// const paragrafos = document.querySelectorAll("p")
+// paragrafos.forEach(paragrafo => {
+//   console.log(paragrafo)
+//   console.log(paragrafo.innerText)
+// });
+
+// // Como corrigir os erros abaixo:
+// const imgs = document.querySelectorAll('img');
+
+// imgs.forEach((item, index) => {
+//   console.log(item, index);
+// });
+
+// let i = 0;
+// imgs.forEach( () => {
+//   console.log(i++);
+// });
+
+// imgs.forEach(() => i++);
+
+// Adicione a classe ativo a todos os itens do menu
+const linksMenu = document.querySelectorAll(".menu a");
+linksMenu.forEach((link) => {
+  link.classList = "ativo";
+  console.log(link);
+});
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+linksMenu.forEach((link) => {
+  link.classList.remove("ativo");
+});
+linksMenu[0].classList.add("ativo");
+
+// Verifique se as imagens possuem o atributo alt
+const imagens = document.querySelectorAll("img");
+imagens.forEach((imagem) => {
+  console.log(imagem.hasAttribute("alt"));
 });
 
-
-
-
-// Como corrigir os erros abaixo:
-const imgs = document.querySelectorAll('img');
-
-imgs.forEach((item, index) => {
-  console.log(item, index);
-});
-
-let i = 0;
-imgs.forEach( () => {
-  console.log(i++);
-});
-
-imgs.forEach(() => i++);
+// Modifique o href do link externo no menu
+linksMenu[linksMenu.length - 1].setAttribute("href", "#");
+console.log(linksMenu[linksMenu.length - 1]);
