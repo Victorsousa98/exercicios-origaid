6ebbc1a768cb6e3b978737f027bda93e7bb2ec90
Victor Sousa <victorsousa708@gmail.com>
2023-05-04 21:28:53 -0300

    ajustes de estilo

--- tabnav/script.js
+++ tabnav/script.js
@@ -5,17 +5,22 @@ const tabDescricao = document.querySelectorAll(
 
 if (tabMenu.length && tabDescricao.length) {
   tabDescricao[0].classList.add("ativo");
-
+  tabMenu[0].classList.add("ativo");
   function activeTab(index) {
     tabDescricao.forEach((descricao) => {
       descricao.classList.remove("ativo");
     });
     tabDescricao[index].classList.add("ativo");
   }
+  function removeAtivoMenu() {
+    tabMenu.forEach((i) => i.classList.remove("ativo"));
+  }
 
   tabMenu.forEach((item, index) => {
     item.addEventListener("click", () => {
       activeTab(index);
+      removeAtivoMenu();
+      item.classList.add("ativo");
     });
   });
 }
